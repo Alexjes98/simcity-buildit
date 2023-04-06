0f96a9642acace59eeab1ffbfcdbfbe0355d377a
Alejandro Lopez <alexjes6543@gmail.com>
2023-04-06 17:48:38 -0400

    roads activate buildings, adjacent position returns an object

--- src/components/game-controller/game_controller.jsx
+++ src/components/game-controller/game_controller.jsx
@@ -4,29 +4,33 @@ import StatsUI from '../../components/ui/stats';
 import Map from '../map/map';
 import Sidebar from '../side-bar/side-bar';
 
-import { defaultMainStats, effectClass } from '../../constants/constants';
-import { __hash__ } from '../../utils/utils';
+import { defaultMainStats } from '../../constants/constants';
+
 import { newPoliceStation, newResidentialZone, newRoad } from '../../constants/defaultBuildings';
 
 export default function GameController() {
 
     const [mainStats, setMainStats] = useState(defaultMainStats)
+    const [buildingsStats, setBuilingsStats] = useState({
+        police: 0,
+        firehouse: 0,
+        hospitals: 0,
+    })
 
-    var mapGrid = Array.from({ length: 15 }, () => Array.from({ length: 15 }, () => { return { name: 'delete', element: {}, effects: [] } }));
-    var selectedElement = { name: "pointer"}
+    const [mapGrid, setMapGrid] = useState(Array.from({ length: 15 }, () => Array.from({ length: 15 }, () => { return { name: 'delete', effects: [] } })));
+    var selectedElement = { name: "pointer" }
     var auxiliarElement = {};
 
     const setSelectedElement = (element) => {
-        if(selectedElement === element) return;
+        if (selectedElement === element) return;
         selectedElement = element;
         return selectedElement;
     }
 
     const setBlock = (xindex, yindex) => {
-        if (selectedElement.name === 'pointer') return console.log(xindex,yindex,mapGrid[xindex][yindex])
-        if (cantBuild(xindex, yindex)) return;  
+        if (selectedElement.name === 'pointer') return console.log(xindex, yindex, mapGrid[xindex][yindex])
+        if (cantBuild(xindex, yindex)) return;
         defineBuildingValues(xindex, yindex);
-        mapGrid[xindex][yindex] = auxiliarElement;
         return selectedElement.name;
     }
 
@@ -41,60 +45,84 @@ export default function GameController() {
         switch (selectedElement.name) {
             case 'road':
                 auxiliarElement = newRoad()
-                definePositionEffects(xindex, yindex);
-                //TODO ACTIVATE ADJACENT BUILDINGS IF INACTIVE
+                auxiliarElement.effects = definePositionEffects(auxiliarElement.effects,mapGrid[xindex][yindex].effects);
+                activateAdjacentBuildings(xindex, yindex);
                 break;
             case 'residential':
                 auxiliarElement = newResidentialZone()
                 if (checkRoads(xindex, yindex)) {
-                    auxiliarElement.setIsActive(true)
-                    definePositionEffects(xindex, yindex);
-                    console.log(auxiliarElement.notCoveredServices())
+                    auxiliarElement.is_active = true
+                    auxiliarElement.effects = definePositionEffects(auxiliarElement.effects,mapGrid[xindex][yindex].effects);
                 }
                 break;
             case 'police':
                 auxiliarElement = newPoliceStation()
                 if (checkRoads(xindex, yindex)) {
-                    addEffects(xindex, yindex, auxiliarElement.cover_area)
+                    setService(xindex, yindex, auxiliarElement.cover_area,auxiliarElement.cover_service)
+                    setBuilingsStats({...buildingsStats, police: buildingsStats.police + 1})
                 }
                 break;
             default:
                 break;
         }
-        console.log("created ", auxiliarElement);
-
+        updateGrid(xindex, yindex, auxiliarElement)
+        //console.log("created ", mapGrid[xindex][yindex]);
     }
-    
+
+    const updateGrid = (xindex, yindex, element) => {
+        var newGrid = mapGrid;
+        newGrid[xindex][yindex] = element;
+        setMapGrid(newGrid)
+    }
+
+    const setService = (xindex, yindex, cover_area, cover_service) => {
+        addEffects(xindex, yindex, cover_area, cover_service)
+        auxiliarElement.is_active = true
+    }
+
+    const activateAdjacentBuildings = (xindex, yindex) => {
+        const adjacentPositions = checkAdjacentPositions(xindex, yindex)
+        const adjacentBuildings = adjacentPositions.filter(element => element.object.type_id !== undefined)
+        adjacentBuildings.forEach(element => {
+            console.log("ACTIVANDO", element.object)
+            addEffects(element.x, element.y, element.object.cover_area, element.object.cover_service)
+            element.object.is_active = true;
+            console.log(element.object.effects)
+            updateGrid(element.x, element.y,element.object);
+        });
+    }
+
     const checkRoads = (xindex, yindex) => {
         const adjacentPositions = checkAdjacentPositions(xindex, yindex)
-        if (adjacentPositions.some(item => item.name === 'road')) {
+        if (adjacentPositions.some(item => item.object.name === 'road')) {
             return true;
         }
         return false;
     }
 
-    const definePositionEffects = (xindex, yindex) => {
-        let combinedEffects = auxiliarElement.effects.concat(mapGrid[xindex][yindex].effects);
-        auxiliarElement.effects = combinedEffects;
+    const definePositionEffects = (targetEffects, sourceEffects) => {
+        let combinedEffects = targetEffects.concat(sourceEffects)
+        return combinedEffects;
     }
 
-    
-    function addEffects(x, y, cover_area) {
-      for (let i = x - cover_area; i <= x + cover_area; i++) {
-        for (let j = y - cover_area; j <= y + cover_area; j++) {
-          if (i >= 0 && i < mapGrid.length-1 && j >= 0 && j < mapGrid[0].length-1) {
-            addMapBoxEffect(i,j);
-          }
+    function addEffects(x, y, cover_area, effect) {
+        for (let i = x - cover_area; i <= x + cover_area; i++) {
+            for (let j = y - cover_area; j <= y + cover_area; j++) {
+                if (i >= 0 && i < mapGrid.length - 1 && j >= 0 && j < mapGrid[0].length - 1) {
+                    addMapBoxEffect(i, j, effect);
+                }
+            }
         }
-      }
     }
 
-    const addMapBoxEffect = (xindex, yindex) => {
-        mapGrid[xindex][yindex].effects.push(auxiliarElement.cover_service)
+    const addMapBoxEffect = (xindex, yindex, effect) => {
+        const newGrid = mapGrid;
+        newGrid[xindex][yindex].effects.push(effect)
+        setMapGrid(newGrid);
     }
 
     const addGamePopulation = () => {
-        const newPopulation = { ...mainStats, population: mainStats.population + selectedElement.class.actual_population }
+        const newPopulation = { ...mainStats, population: mainStats.population + selectedElement.actual_population }
         setMainStats(newPopulation)
     }
 
@@ -106,22 +134,22 @@ export default function GameController() {
         if (xindex < 0 || xindex >= mapGrid.length || yindex < 0 || yindex >= mapGrid[0].length) {
             throw new Error("Index out of bounds");
         }
-        const adjacentPositions = [];
+        const adjacentPositions = [{object: {}, x: {}, y:{}}];
         // check top position
         if (xindex > 0) {
-            adjacentPositions.push(mapGrid[xindex - 1][yindex]);
+            adjacentPositions.push({object: mapGrid[xindex - 1][yindex], x: xindex-1, y: yindex});            
         }
         // check bottom position
         if (xindex < mapGrid.length - 1) {
-            adjacentPositions.push(mapGrid[xindex + 1][yindex]);
+            adjacentPositions.push({object: mapGrid[xindex + 1][yindex], x: xindex+1,y: yindex});
         }
         // check left position
         if (yindex > 0) {
-            adjacentPositions.push(mapGrid[xindex][yindex - 1]);
+            adjacentPositions.push({object: mapGrid[xindex][yindex - 1], x: xindex, y: yindex-1});
         }
         // check right position
         if (yindex < mapGrid[0].length - 1) {
-            adjacentPositions.push(mapGrid[xindex][yindex + 1]);
+            adjacentPositions.push({object: mapGrid[xindex][yindex + 1], x: xindex, y: yindex +1});
         }
         return adjacentPositions;
     }
